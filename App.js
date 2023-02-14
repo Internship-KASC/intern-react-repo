5377fe0e08bc511046a0bc7a97dfe33d47a2ab4d
tharani3920 <122532168+tharani3920@users.noreply.github.com>
2023-02-14 10:17:22 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './footer/Footer';
+import Header from './header/Header';
+
+function App() {
+  return (
+   <div>
+    <Header />
+    <Footer />
+   </div>
+  );
+}
+
+export default App;
