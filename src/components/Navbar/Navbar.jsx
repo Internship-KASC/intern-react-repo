efa74d1e648b1e6aa8d8fa65f33e18219f578cd9
PraveenTK <117259042+PraveenTK@users.noreply.github.com>
2023-02-14 20:55:39 +0530

    Made changes in the Navbar with responsive Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
-import './Navbar-style.css';
+import React ,{useState} from 'react';
+import { Link } from "react-router-dom";
+import './Navbar.css';
+import {FaBars,FaTimes} from 'react-icons/fa';
 
 const Navbar = () => {
+    const [icon, setIcon] = useState(false);
+    const handleClick = () => {
+        setIcon(!icon)
+    }
+    const closeSideDrawer = () => {
+        icon(false)
+    }
   return (
-    <nav className="navbar">
-      <div className="logo">
-        <a href='#'><h1>My<span>Quote</span></h1></a>
-      </div>
-      <div className="nav-menu">
-        <ul>
-          <li className="nav-items"><a href="#">Home</a></li>
-          <li className="nav-items"><a href="#">About</a></li>
-          <li className="nav-items"><a href="#">Requirements</a></li>
-          <li className="nav-items"><a href="#">Blog</a></li>
-          <li className="nav-items"><a href="#">Contact</a></li>
+    <div>
+    <nav className='navbar'>
+        <Link to='/' className='nav-logo' onClick={closeSideDrawer}><h1>My<span>Quote</span></h1></Link>
+        <div className='menu-icon' onClick={handleClick}>
+            {
+                icon ? <FaTimes className='fa-times'></FaTimes> : <FaBars className='fa-bars'></FaBars>
+            }
+        </div>
+        <ul className={
+            icon ? 'nav-menu active' : 'nav-menu'
+        }>
+            <li><Link to='/' className='nav-links' onClick={closeSideDrawer}>Home</Link></li>
+            <li><Link to='/About' className='nav-links' onClick={closeSideDrawer}>About Us</Link></li>
+            <li><Link to='/Services' className='nav-links' onClick={closeSideDrawer}>Services</Link></li>
+            <li><Link to='/Requirement' className='nav-links' onClick={closeSideDrawer}>Requirement</Link></li>
+            <li><Link to='/Terms' className='nav-links' onClick={closeSideDrawer}>Terms of Use</Link></li>
+            <li><Link to='/Contact' className='nav-links' onClick={closeSideDrawer}>Contact-Us</Link></li>
         </ul>
-      </div>
     </nav>
+    </div>
   )
 }
 
