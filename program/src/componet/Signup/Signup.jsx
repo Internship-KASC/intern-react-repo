c53e4d30c1f8fc6451f2e85354e5760d2e9c85ba
abimani1 <122529930+abimani1@users.noreply.github.com>
2023-02-14 21:52:57 +0530

    sign

---
+++ program/src/componet/Signup/Signup.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+        <div className="continer">
+            <div className="row-2">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Signup
