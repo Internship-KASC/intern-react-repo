d5bcff40744e097c41836c40529b25b3e866f59d
vinodhini060 <vinohinii06@gmail.com>
2023-03-15 18:02:17 +0530

    update

--- myquite/website/src/components/Navbar.js
+++ myquite/website/src/components/Navbar.js
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import { FaTimes, FaBars } from "react-icons/fa";
 import "./Navbar.css"
+import { Button } from './Button';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
     const handleClick = () => setClick(click);
     const closeMobileMenu = () => setClick(false);
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+
+        }
+    };
+
+    useEffect(() => {
+        showButton();
+    }, []);
+    window.addEventListener('resize', showButton);
     return (
         <>
             <nav className="navbar">
@@ -44,6 +59,7 @@ function Navbar() {
                             </Link>
                         </li>
                     </ul>
+                    {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
 
                 </div>
             </nav>
